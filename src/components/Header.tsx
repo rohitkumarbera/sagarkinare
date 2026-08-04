37471693f19daecdae19e715bd68183d34f18b87
Ayan Giri <ayangiri2006@gmail.com>
2026-08-04 14:02:14 +0530

    Replace weather widget in top bar with official Instagram, Facebook, and Youtube SVG icons linking directly to Sagar Kinare social channels

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Phone, Mail, Menu, X, Calendar, ShieldCheck, MapPin, Instagram, Facebook, Youtube } from 'lucide-react';
+import { Phone, Mail, Menu, X, Calendar, ShieldCheck, MapPin } from 'lucide-react';
 import { HOTEL_INFO, ORIGINAL_IMAGES } from '../data/resortData';
 import { LuxuryBookingModal } from './LuxuryBookingModal';
 
@@ -81,7 +81,7 @@ export const Header: React.FC = () => {
 
             {/* RIGHT: Social Media Links (Instagram, Facebook, Youtube) */}
             <div className="flex items-center gap-4">
-              <div className="hidden sm:flex items-center gap-1.5 text-[#F8F6F2]/80 font-medium mr-2">
+              <div className="hidden sm:flex items-center gap-1.5 text-[#F8F6F2]/80 font-medium mr-1">
                 <MapPin className="w-3.5 h-3.5 text-[#C8A45A]" />
                 <span>Tajpur Beach</span>
               </div>
@@ -89,6 +89,7 @@ export const Header: React.FC = () => {
               <span className="hidden sm:inline text-[#C8A45A]/40">•</span>
 
               <div className="flex items-center gap-3">
+                {/* Instagram */}
                 <a
                   href={HOTEL_INFO.socials.instagram}
                   target="_blank"
@@ -96,9 +97,12 @@ export const Header: React.FC = () => {
                   aria-label="Sagar Kinare Instagram"
                   className="p-1 rounded-full text-[#F8F6F2] hover:text-[#C8A45A] hover:scale-110 transition-all duration-300"
                 >
-                  <Instagram className="w-4 h-4" />
+                  <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                  </svg>
                 </a>
 
+                {/* Facebook */}
                 <a
                   href={HOTEL_INFO.socials.facebook}
                   target="_blank"
@@ -106,9 +110,12 @@ export const Header: React.FC = () => {
                   aria-label="Sagar Kinare Facebook"
                   className="p-1 rounded-full text-[#F8F6F2] hover:text-[#C8A45A] hover:scale-110 transition-all duration-300"
                 >
-                  <Facebook className="w-4 h-4" />
+                  <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9 8H6v4h3v12h5V12h3.642L18 8h-4V6.333C14 5.374 14.5 5 15.5 5H18V0h-3.808C10.592 0 9 1.592 9 4.415V8z"/>
+                  </svg>
                 </a>
 
+                {/* Youtube */}
                 <a
                   href={HOTEL_INFO.socials.youtube}
                   target="_blank"
@@ -116,7 +123,9 @@ export const Header: React.FC = () => {
                   aria-label="Sagar Kinare Youtube"
                   className="p-1 rounded-full text-[#F8F6F2] hover:text-[#C8A45A] hover:scale-110 transition-all duration-300"
                 >
-                  <Youtube className="w-4 h-4" />
+                  <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
+                  </svg>
                 </a>
               </div>
             </div>
